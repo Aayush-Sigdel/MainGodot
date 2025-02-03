d98719e8d6d3056ea2447bda3cbeab5600b80e8b
Aayush Sigdel <itsforaayush@gmail.com>
2025-02-03 15:49:26 +0545

    02- DragAndDrop

--- 02- DragDrop/Script/playertank.cs
+++ 02- DragDrop/Script/playertank.cs
@@ -1,17 +1,20 @@
 using Godot;
+using System;
 
-public partial class Tank : CharacterBody2D
+public partial class playertank : CharacterBody2D
 {
 	private float _speed = 300f;
-   [Export] NavigationAgent2D _navigationAgent;
+	[Export] NavigationAgent2D _navigationAgent;
+	public Boolean _mouseOver = false;
+	[Export] public CollisionShape2D _collisionShape;
     
-   	public override void _Process(double delta)
+	public override void _Process(double delta)
 	{
 		//player rotation
 		Vector2 targetPosition = _navigationAgent.GetNextPathPosition();
 		Vector2 direction = (targetPosition - GlobalPosition).Normalized();
 		var targetRotation = direction.Angle();
-		Rotation = Mathf.LerpAngle(Rotation, targetRotation, 10.0f * (float)delta);
+		 Rotation = Mathf.LerpAngle(Rotation, targetRotation, 10.0f * (float)delta);
 		
 		// navigating through navigation field
 		if (Input.IsActionJustPressed("mouse.left"))
@@ -21,11 +24,25 @@ public partial class Tank : CharacterBody2D
 			Vector2 closestPoint = NavigationServer2D.MapGetClosestPoint(map, p);
 			_navigationAgent.TargetPosition = closestPoint;	
 		}
+
+		if (_mouseOver && Input.IsActionPressed("mouse.right") == true)
+		{
+			Position = GetGlobalMousePosition();
+			_collisionShape.Scale = new Vector2(64f, 64f);
+		}
+		else
+		{
+			_collisionShape.Scale = new Vector2(6f, 6f);
+		}
+		
+		
+		
+		
 		MoveTowards();
 		
 	}
 
-    //Character Movement
+	//Character Movement
 	private void MoveTowards()
 	{
 		if(_navigationAgent.IsNavigationFinished()) return;
@@ -34,5 +51,17 @@ public partial class Tank : CharacterBody2D
 		Vector2 direction = diff.Normalized();
 		Velocity = direction * _speed;
 		MoveAndSlide();
+		GD.Print("Velocity: " + Velocity);
+	}
+
+	public void _on_mouse_entered()
+	{
+		GD.Print("Mouse entered");
+		_mouseOver = true;
+	}
+
+	public void _on_mouse_exited()
+	{	GD.Print("Mouse exited");
+		_mouseOver = false;
 	}
 }
